77532d632572dcac33141a37b298d6fe4c06abc9
Hitesh Kuber <hiteshkuber@gmail.com>
2024-12-07 21:10:22 +0530

    organised

--- main.py
+++ main.py
@@ -8,59 +8,65 @@ class MyGUI(QMainWindow):
 
     def __init__(self):
         super(MyGUI, self).__init__()
+        self.setup_ui()
+        self.setup_window_control_button_positions()
+        self.setup_window_control_button_control()
+        self.setup_edit_menu()
+
+    def setup_ui(self):
+        """Load the UI and setup initial window properties."""
         uic.loadUi('editor.ui', self)
 
         # Remove the title bar and border
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.show()
 
-        self.setup_edit_menu()
-        self.setup_window_control_button_positions()
-        self.setup_window_control_button_control()
-
     def setup_edit_menu(self):
+        """Connect actions in the edit menu to the appropriate methods."""
         self.actionFont_12pt.triggered.connect(lambda: self.change_size(12))
         self.actionFont_18pt.triggered.connect(lambda: self.change_size(18))
         self.actionFont_24pt.triggered.connect(lambda: self.change_size(24))
 
     def change_size(self, size: int):
+        """Change the font size of the text editor."""
         self.textEdit.setFont(QFont('Arial', size))
 
     def setup_window_control_button_positions(self):
-        # getting the right most of the window
+        """Position the window control buttons (minimize, maximize, close)."""
         new_x = self.get_right_corner()
 
-        # moving the buttons to top right of the screen
+        # Position buttons at the top-right corner
         self.minimizeButton.move(new_x - 70, 6)
         self.maximizeButton.move(new_x - 40, 6)
         self.closeButton.move(new_x - 10, 6)
 
     def setup_window_control_button_control(self):
+        """Connect window control buttons to their respective actions."""
         self.minimizeButton.clicked.connect(self.minimize_screen)
         self.maximizeButton.clicked.connect(self.maximize_screen)
         self.closeButton.clicked.connect(self.close_screen)
 
     def maximize_screen(self):
+        """Maximize the window."""
         self.showMaximized()
 
     def minimize_screen(self):
+        """Minimize the window."""
         self.showMinimized()
 
     def close_screen(self):
+        """Close the window."""
         self.close()
 
     def get_right_corner(self):
-        # Get the size of the window
+        """Calculate the x-coordinate for positioning buttons at the top-right."""
         window_width = self.width()
-
-        # Get the size of the maximize button (this assumes you set it in the UI file)
         button_width = self.maximizeButton.width()
-
-        # Calculate the new position for the top-right corner
         return window_width - button_width - 10  # 10px margin from the right
 
 
 def main():
+    """Initialize and run the application."""
     app = QApplication([])
     window = MyGUI()
     app.exec()
